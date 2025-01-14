2a0fea402cfd6cafe3239536ad86cd28ef94e31d
Aditya Malu <C2K21107036@ms.pict.edu>
2025-01-14 14:11:14 +0530

    Signals, FrontendUpadted

--- Project 1/project1/food/models.py
+++ Project 1/project1/food/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 class Item(models.Model):
@@ -6,8 +8,13 @@ class Item(models.Model):
     def __str__(self):
         return self.item_name
     
+    user_name = models.ForeignKey(User,on_delete=models.CASCADE,default=1)
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
-    item_image = models.CharField(max_length=1000,default="https://www.istockphoto.com/photos/pizza")
+    item_image = models.CharField(max_length=1000,default="https://media.istockphoto.com/id/938742222/photo/cheesy-pepperoni-pizza.jpg?s=1024x1024&w=is&k=20&c=OKXH55QwDa6L3cY2pTTz1DKVT2clqW3zcVaJVaU-N_U=")
 
+
+    def get_absolute_url(self):
+        return reverse("food:detail", kwargs={"pk": self.pk})
+    
--- Project 1/project1/food/views.py
+++ Project 1/project1/food/views.py
@@ -4,6 +4,9 @@ from django.http import HttpResponse
 from food.forms import ItemForm
 from .models import Item
 from django.template import loader
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 # Create your views here.
 def index(request):
     item_list = Item.objects.all()
@@ -12,6 +15,11 @@ def index(request):
         'item_list' : item_list,
     }
     return render(request,'food/index.html',context)
+
+class IndexClassView(ListView):
+    model = Item
+    template_name = 'food/index.html'
+    context_object_name = 'item_list'
 
 def item(request):
     return HttpResponse('this is an item view')
@@ -23,12 +31,27 @@ def detail(reuqest,item_id):
     }
     return render(reuqest,'food/detail.html',context)
 
+class FoodDetail(DetailView):
+    model = Item
+    template_name = 'food/detail.html'
+
 def create_item(request):
     form = ItemForm(request.POST or None)
     if form.is_valid():
         form.save()
         return redirect('food:index')
     return render(request,'food/item_form.html',{'form':form})
+
+class Create_Item(CreateView):
+    model = Item
+    fields = ['item_name','item_desc','item_price','item_image']
+    template_name = 'food/item_form.html'
+
+    def form_valid(self, form):
+        form.instance.user_name = self.request.user
+        return super().form_valid(form)
+    
+
 
 def update_items(request,id):
     item = Item.objects.get(id=id)
